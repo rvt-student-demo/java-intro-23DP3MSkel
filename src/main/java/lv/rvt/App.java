646bfbbcf8d7dac2e1d25bc761932d2c790d12ec
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-09-18 07:02:06 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,26 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
         // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        
+        String tekstsPirmais = scanner.nextLine();
+        int pirmaisSkaitlis = Integer.valueOf(tekstsPirmais);
+        
+        System.out.println("Give the second number:");
+        String tekstsOtrais = scanner.nextLine();
+        int otraisSkaitlis = Integer.valueOf(tekstsOtrais);
 
+        double dalishana = (double) pirmaisSkaitlis / otraisSkaitlis;
+
+        System.out.println(tekstsPirmais + "+" + tekstsOtrais + "=" + (pirmaisSkaitlis + otraisSkaitlis));
+        System.out.println(tekstsPirmais + "-" + tekstsOtrais + "=" + (pirmaisSkaitlis-otraisSkaitlis));
+        System.out.println(tekstsPirmais + "*" + tekstsOtrais + "=" +(pirmaisSkaitlis * otraisSkaitlis));
+        System.out.println(tekstsPirmais + "/" + tekstsOtrais + "=" + dalishana);
     }
 
 }
