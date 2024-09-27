6de45e0812358e9dac953cbdd554599a8b401d31
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-09-27 07:33:37 +0000

    gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,5 +1,4 @@
 package lv.rvt;
-import java.nio.channels.InterruptibleChannel;
 import java.util.Scanner;
 public class App 
 {
@@ -7,28 +6,22 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Iedo punktus (0-100)");
-        int punkti = Integer.valueOf(scanner.nextLine());
+        System.out.println("whats the value of the gift?");
+        int nauda = Integer.valueOf(scanner.nextLine());
 
-        if (punkti<0){
-            System.out.println("Nav iespējams");
-        } else if (punkti < 49) {
-            System.out.println("fails");
-        } else if (punkti>50 && punkti<59){
-            System.out.println("1");
-        } else if (punkti>60 && punkti<69){
-            System.out.println("2");
-        } else if (punkti>70 && punkti<79){
-            System.out.println("3");
-        } else if (punkti>80 && punkti<89){
-            System.out.println("4");
-        } else if (punkti>90 && punkti<100){
-            System.out.println("5");
-        } else if (punkti>100){
-            System.out.println("neticami");
-        }
-    
-    scanner.close();
+        if (nauda<5000){
+            System.out.println("0% tax");
+        }else if (nauda>=5000 && nauda<25000){
+            System.out.println("tax: "+ (100 + (nauda-5000) * 0.08));
+        }else if(nauda>=25000 && 55000>nauda){
+        System.out.println("tax: "+(1700 + (nauda-25000)*0.1));
+    }else if (nauda>=55000 && nauda<200000){
+        System.out.println("tax: "+(4700+(nauda-55000)*0.12));
+    }else if (nauda>=200000 && nauda<1000000){
+        System.out.println("tax: "+ (22100 + (nauda-200000)*0.15));
+    } else if (nauda>=1000000){
+        System.out.println("tax: " +(142100 + (nauda-1000000)*0.17));
+    }
     
     }
 
