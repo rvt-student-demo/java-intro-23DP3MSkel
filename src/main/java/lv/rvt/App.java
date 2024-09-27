f024d6120d4624f2c76c28230bb48bfba4247d90
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-09-27 07:55:52 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,24 +5,25 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("whats the value of the gift?");
-        int nauda = Integer.valueOf(scanner.nextLine());
+   
+        int numbers = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("give a number:");
+            int nulle = Integer.valueOf(scanner.nextLine());
+            if(nulle != 0){
+                numbers = numbers+1;
+                sum = sum + nulle;
+                continue;
+            }
+            if (nulle == 0){
+                break;
+            }
+            
 
-        if (nauda<5000){
-            System.out.println("0% tax");
-        }else if (nauda>=5000 && nauda<25000){
-            System.out.println("tax: "+ (100 + (nauda-5000) * 0.08));
-        }else if(nauda>=25000 && 55000>nauda){
-        System.out.println("tax: "+(1700 + (nauda-25000)*0.1));
-    }else if (nauda>=55000 && nauda<200000){
-        System.out.println("tax: "+(4700+(nauda-55000)*0.12));
-    }else if (nauda>=200000 && nauda<1000000){
-        System.out.println("tax: "+ (22100 + (nauda-200000)*0.15));
-    } else if (nauda>=1000000){
-        System.out.println("tax: " +(142100 + (nauda-1000000)*0.17));
-    }
-    
+        }
+        System.out.println("Number of numbers:"+numbers);
+        System.out.println("Sum of the numbers:"+sum);
     }
 
 }
