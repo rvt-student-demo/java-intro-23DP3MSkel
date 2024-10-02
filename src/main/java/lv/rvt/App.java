07e214875d221bef6b94834103854cd94dc2c142
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-10-02 07:44:32 +0000

    RePrint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,36 +5,16 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        int reizes = 0;
    
-        int sum = 0;
-        int amount = 0;
-        int even = 0;
-        int odd = 0;
-
-        while(true){
-        System.out.println("Give numbers:");
-        int skaitlis = Integer.valueOf(scanner.nextLine());
-        if(skaitlis>-1 || skaitlis<-1){
-            sum = sum +  skaitlis;
-            amount = amount + 1;
-            if(skaitlis % 2 == 0){
-                even ++;
-            }else if (skaitlis % 2 != 0){
-                odd = odd + 1;
-                
-            }
-        }else if (skaitlis == -1){
-            break;
+        int lietotajs = Integer.valueOf(scanner.nextLine());
+        while(reizes<lietotajs ){
+            reizes +=1;
+            text();
         }
-    }
-
-    System.out.println("Thx! Bye!");
-    System.out.println("Sum: "+ sum);
-    System.out.println("Numbers: "+ amount);
-    System.out.println("average: "+ (double) sum/amount);
-    System.out.println("Even "+ even);
-    System.out.println("ODD: "+ odd);
 }
-
+public static void text(){
+    System.out.println("In a hole in the ground there lived a method");
+}
     
 }
