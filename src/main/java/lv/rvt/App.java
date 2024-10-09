f608ee73a01d837b23013d949c53d8c8f869335a
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-10-09 06:33:38 +0000

    DivisibleByThree bit modified

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,9 +8,6 @@ public class App
         divisibleByThree(2,10);
 
     }
-    public static void divide(int pirm, int otr){
-        System.out.println((double)pirm/otr);
-    }
     public static void divisibleByThree(int first, int second){
         while(first<=second){
             if(first % 3 == 0){
