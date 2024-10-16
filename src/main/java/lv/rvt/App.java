6ca09d0ec76314b3d86b5e65d9da308e3674aa21
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-10-16 07:45:03 +0000

    List Summa

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,30 @@
 package lv.rvt;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class App 
 {
     public static void main( String[] args )
     {
-        Stars.printTriangle(4);
-
+        int summa=0;
+        int a=1;
+        Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> skaitli = new ArrayList<>();
+        while (a==1) {
+            System.out.println("Ievadiet");
+            int input =Integer.valueOf(scanner.nextLine());
+                skaitli.add(input);
+            if (input==0) {
+                a=0;
+                for (int i = 0; i <skaitli.size(); i++) {
+                    summa=summa+skaitli.get(i);
+                    
+           
+                }
+                System.out.println();
+                System.out.println(summa);
+           }   
+        }
 
     }
     
