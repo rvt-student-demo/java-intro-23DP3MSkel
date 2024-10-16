5fa2a834f549cf2163eb48610ac46a780860799e
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-10-16 07:14:01 +0000

    triangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stars.PrintSqare(6);
+        Stars.printTriangle(4);
 
 
     }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -11,9 +11,33 @@ public static void PrintStars(int number){
     System.out.println("");
     }
 
-public static void PrintSqare(int number){
-    for(int x =0;number>x;x++){
+ public static void printRectangle(int number, int height ){
+    for(int x =0;height>x;x++){
         PrintStars(number);
     }
 }
+
+public static void printTriangle(int tristuris){
+
+    int sakums = 1;
+    while(sakums<tristuris+1){
+        printSpaces(tristuris-sakums);
+        PrintStars(sakums);
+        sakums ++;
+        
+    }
+
+}
+
+
+public static void printSpaces(int number){
+    for(int x=0;number>x++;){
+        System.out.print(" ");
+    }
+}
+
+public static void printTriangle2(int number){
+
+}
+
 }
