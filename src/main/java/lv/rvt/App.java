0a132d213dcc9c429ddf97388c89e104a8c1995a
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-10-25 07:30:20 +0000

    Sum of list

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,23 +6,23 @@ public class App
     public static void main( String[] args )
     {
         int summa=0;
-        int a=1;
+        int a=0;
         Scanner scanner = new Scanner(System.in);
         
         ArrayList<Integer> skaitli = new ArrayList<>();
-        while (a==1) {
+        while (a==0) {
             System.out.println("Ievadiet");
             int input =Integer.valueOf(scanner.nextLine());
                 skaitli.add(input);
-            if (input==0) {
-                a=0;
+            if (input==-1) {
+                a=-1;
                 for (int i = 0; i <skaitli.size(); i++) {
                     summa=summa+skaitli.get(i);
                     
            
                 }
                 System.out.println();
-                System.out.println(summa);
+                System.out.println("Sum: "+summa);
            }   
         }
 
